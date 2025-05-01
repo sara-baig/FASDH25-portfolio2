0e55e99e8df1e1450c44476203d1c673b78d80b9
ZehrishIqbal <zehrish.iqbal@scholar.aku.edu>
2025-05-01 16:10:43 +0500

    added new scripts folder

---
+++ Scripts/regex_script_zehrish_iqbal_6.py
@@ -0,0 +1,22 @@
+'''This is your starting script for today's Python class.
+
+This script contains the code we wrote last week
+to count the number of times each place in Gaza
+is mentioned in our corpus.
+
+Now, we want to store this count into a tsv file.
+
+I have written a function (write csv) to do this -
+but it has some mistakes in it.
+
+Please fix the mistakes and call the function
+to write the 
+
+'''
+import re
+import os
+import pandas as pd
+
+
+#Define folders
+
